867fba815f531cd52adcbeae64bb8563d8420670
Serhiy Bardysh <sbardysh@tutanota.com>
2020-07-26 18:32:38 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -17,7 +17,7 @@ class Board
     @board = board || Array.new(9, " ")
   end
 
-  def display
+  def self.display
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
