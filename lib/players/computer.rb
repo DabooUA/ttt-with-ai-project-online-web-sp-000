d713da6b0c94b034d2c152164719588371d876a1
Serhiy Bardysh <sbardysh@tutanota.com>
2020-07-26 19:54:25 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,24 +3,24 @@ module Players
     class Computer < Player
 
       def move(board)
-      move = nil
+        move = nil
 
-      if !board.taken?(5)
-        move = "5"
-      elsif board.turn_count == 1
-        move = "1"
-      elsif board.turn_count == 2
-        move = [1, 3, 7, 9].detect{ |i| !board.taken?(i) }.to_s
-      elsif board.turn_count == 3 && (board.position(9) == board.position(1) || board.position(3) == board.position(7))
-        move = "2" unless board.taken?("2")
-        move ? nil : move = "3"
-      else
-        Game::WIN_COMBINATIONS.detect do |combo|
-          if combo.select{ |i| board.position(i + 1) == token }.size == 2 && combo.any?{ |i| board.position(i+1) == " " }
-            move = combo.select{ |i| !board.taken?(i + 1) }.first.to_i.+(1).to_s
-          elsif combo.select{ |i| board.position(i + 1) != " " && board.position(i + 1) != token }.size == 2 && combo.any?{ |i| board.position(i + 1) == " " }
-            move = combo.select{ |i| !board.taken?(i + 1) }.first.to_i.+(1).to_s
-          end
+        if !board.taken?(5)
+          move = "5"
+        elsif board.turn_count == 1
+          move = "1"
+        elsif board.turn_count == 2
+          move = [1, 3, 7, 9].detect{ |i| !board.taken?(i) }.to_s
+        elsif board.turn_count == 3 && (board.position(9) == board.position(1) || board.position(3) == board.position(7))
+          move = "2" unless board.taken?("2")
+          move ? nil : move = "3"
+        else
+          Game::WIN_COMBINATIONS.detect do |combo|
+            if combo.select{ |i| board.position(i + 1) == token }.size == 2 && combo.any?{ |i| board.position(i+1) == " " }
+              move = combo.select{ |i| !board.taken?(i + 1) }.first.to_i.+(1).to_s
+            elsif combo.select{ |i| board.position(i + 1) != " " && board.position(i + 1) != token }.size == 2 && combo.any?{ |i| board.position(i + 1) == " " }
+              move = combo.select{ |i| !board.taken?(i + 1) }.first.to_i.+(1).to_s
+            end
         end
 
         # If none of the WIN_COMBINATIONS patterns have two squares taken by the same token and a third empty square, play into the first open square you find, first checking corners and then checking sides.
