c66bc2bfa3824336ba849c6bfa0e58ad82bed9aa
Serhiy Bardysh <sbardysh@tutanota.com>
2020-07-26 19:24:29 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -14,6 +14,7 @@ class Game
                     [0,4,8],
                     [2,4,6]
                   ]
+                  
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @board = board
     @player_1 = player_1
@@ -23,7 +24,7 @@ class Game
   def move(index, current_player)
     @board[index] = current_player
   end
-  
+
   def current_player
     @board.turn_count % 2 == 0 ? player_1 : player_2
   end
