347c806a3558334cd60a76cfa88f242f9a0733d9
Serhiy Bardysh <sbardysh@tutanota.com>
2020-07-26 18:49:29 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -37,7 +37,7 @@ class Board
 
   def valid_move? (index)
     !taken?(index) && index.to_i.between?(1, 9)
-    index.between?(0,8) && !position_taken?(index)
+    #index.between?(0,8) && !position_taken?(index)
   end
 
 
