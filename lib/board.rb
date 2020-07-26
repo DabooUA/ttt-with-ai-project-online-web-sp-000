1112c35219c44bac99c1f4896750d6508246bd2b
Serhiy Bardysh <sbardysh@tutanota.com>
2020-07-26 18:45:10 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -28,14 +28,7 @@ class Board
   end
 
   def turn_count
-    cells.count{|c| c == "X" || c == "O"}
-      count = 0
-      @board.each do |spaces|
-        if spaces == "X" || spaces == "O"
-          count += 1
-        end
-      end
-    return count
+    @cells.count{|c| c == "X" || c == "O"}
   end
 
   def position(index)
