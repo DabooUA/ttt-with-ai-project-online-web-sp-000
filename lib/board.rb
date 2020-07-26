801a09dc5747aaf2748e3df39ef525536d98de3a
Serhiy Bardysh <sbardysh@tutanota.com>
2020-07-26 18:31:16 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -17,13 +17,13 @@ class Board
     @board = board || Array.new(9, " ")
   end
 
-  def display_board
+  def display
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-    @display_board
+    @display
   end
 
 end
