fdfc19be741e878f6553e05f8c20dcd0d3ab35cf
Serhiy Bardysh <sbardysh@tutanota.com>
2020-07-26 19:13:26 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -14,8 +14,10 @@ class Game
                     [0,4,8],
                     [2,4,6]
                   ]
-  def initialize
-
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
   end
 
   def won?
